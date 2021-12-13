0a62e0833c22c5415a560ca44e5cccc54e760bc9
ody <no email>
2021-12-12 16:31:07 -0800

    missed git ignore...

--- recipes/target/debug/deps/recipes.d
+++
@@ -1,6 +0,0 @@
-D:\src_repos\cookbook\recipes\target\debug\deps\recipes.exe: src\main.rs src\random.rs
-
-D:\src_repos\cookbook\recipes\target\debug\deps\recipes.d: src\main.rs src\random.rs
-
-src\main.rs:
-src\random.rs:
--- recipes/target/debug/recipes.d
+++
@@ -1,1 +0,0 @@
-D:\src_repos\cookbook\recipes\target\debug\recipes.exe: D:\src_repos\cookbook\recipes\src\main.rs D:\src_repos\cookbook\recipes\src\random.rs
