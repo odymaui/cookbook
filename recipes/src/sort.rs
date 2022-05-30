6ab64c2b7cf13ae454b8458c04bc248ce0450939
ody <no@email.foo>
2022-05-30 10:37:35 -0700

    clean up approach leverage syntax

--- recipes/src/sort.rs
+++ recipes/src/sort.rs
@@ -1,77 +1,55 @@
 //use partial order to support floats.
 //copy or clone needed for .to_vec()
-pub fn merge_sort<T: PartialOrd + Copy>(items: &mut Vec<T>) {
-    //handle bounds interally    
+
+pub fn merge_sort<T: PartialOrd + Copy>(items: &mut [T]) {
+
     if items.len() == 0 {
         return;
     }
 
-    let left: usize = 0;
-    let right: usize = items.len() - 1;
+    if items.len() > 1 {
+            let mid = items.len()/2;
 
-    merge_sort_internal(items, left, right);
-}
-
-fn merge_sort_internal<T: PartialOrd + Copy>(items: &mut Vec<T>, left: usize, right: usize) {
-
-    let mid: usize;
-
-    if left < right {
-        mid = ((right - left) / 2) + left;
-        if mid >= left && mid <= right {
-            merge_sort_internal(items, left, mid);
-            merge_sort_internal(items, mid + 1, right);
+            merge_sort(&mut items[..mid]);
+            merge_sort(&mut items[mid..]);
         
-            merge(items, left, mid, right);
-        }
+            merge(items, mid);
     }
 }
 
-fn merge<T: PartialOrd + Copy>(items: &mut Vec<T>, left: usize, mid: usize, right: usize) {
+fn merge<T: PartialOrd + Copy>(items: &mut [T], mid: usize) {
     //copy the left and right sides of the array
-    let left_array = copy_array(items, left, mid);
-    let right_array = copy_array(items, mid + 1, right);
+    let left_array = items[..mid].to_vec();
+    let right_array = items[mid..].to_vec();
     
-    let mut leftidx:usize = 0;
-    let mut rightidx:usize = 0;
-    let mut mainidx = left;
-    //guard the bounds access
-    let idx_chk = |leftidx, rightidx| left_array.len() > leftidx && right_array.len() > rightidx;
+    let mut l_idx:usize = 0;
+    let mut r_idx:usize = 0;
 
-    //loop over array range length while left and right are not done
-    while idx_chk(leftidx, rightidx) && left_array.len() > 0 && right_array.len() > 0 {
-        //compare left to right and swap as needed
-        if idx_chk(leftidx, rightidx) && left_array[leftidx] <= right_array[rightidx] {
-            items[mainidx] = left_array[leftidx];
-            leftidx += 1;
-            mainidx += 1;
+
+    for idx in 0..items.len() {
+        //while in bounds of each array
+        if l_idx < left_array.len() && r_idx < right_array.len() {
+            if left_array[l_idx] < right_array[r_idx] {
+                items[idx] = left_array[l_idx];
+                l_idx += 1;
+            } else {
+                items[idx] = right_array[r_idx];
+                r_idx += 1;
+            }
+        } else {
+            //finally check both left range and right range and add remain to the end...
+            if l_idx < left_array.len() {
+                items[idx] = left_array[l_idx];
+                l_idx += 1;
+            }
+            
+            if r_idx < right_array.len() {
+                items[idx] = right_array[r_idx];
+                r_idx += 1;
+            }
         }
-
-        if idx_chk(leftidx, rightidx) && right_array[rightidx] < left_array[leftidx] {
-            items[mainidx] = right_array[rightidx];
-            rightidx += 1;
-            mainidx += 1;
-        }
-    }
-
-    //finally check both left range and right range and add remain to the end...
-    while leftidx < left_array.len() {
-        items[mainidx] = left_array[leftidx];
-        leftidx += 1;
-        mainidx += 1;
-    }
-    
-    while rightidx < right_array.len() {
-        items[mainidx] = right_array[rightidx];
-        rightidx += 1;
-        mainidx += 1;
     }
 }
-
-fn copy_array<T: PartialOrd + Copy>(items: &Vec<T>, left: usize, mid: usize) -> Vec<T> {
-    items[left..=mid].to_vec()
-}
-
 
 #[cfg(test)]
 mod sort_tests {
@@ -135,13 +113,13 @@ mod sort_tests {
         items.shuffle(&mut rng);
 
         println!("{:?}", items);
-
         super::merge_sort(&mut items);
+
+        //to see output run: cargo test -- --nocapture
+        println!("{:?}", items);
 
         for idx in 0..(100 - 2) {
             assert!(items[idx] <= items[idx + 1]);
         }
-        //to see output run: cargo test -- --nocapture
-        println!("{:?}", items);
     }
 }
